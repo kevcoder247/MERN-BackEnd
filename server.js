fc3d11b0f97e098e68a55a7305aa87bec6cd1300
)- <a-@-s-MacBook-Pro.local>
2022-06-19 11:20:04 -0400

    wip added mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Dependencies
 const express = require('express');
+const mongoose = require('mongoose');
 
 //Initialize the Express App 
 const app = express();
@@ -7,7 +8,15 @@ const app = express();
 //Comfigure app Settings
 require('dotenv').config();
 
-const {PORT  = 4000, MONGODB_URL} = process.env.PORT;
+const {PORT  = 4000, MONGODB_URL} = process.env;
+
+//Connect to mongoDB
+mongoose.connect(MONGODB_URL);
+
+//Mongo Status listeners
+const db = mongoose.connection
+db.on('connected', () => console.log('Connected ot MongoDb'));
+db.on('Error',  (err) => console.log('Error with MongoDb: ' + err.message))
 
 //Mount MiddleWare
 
